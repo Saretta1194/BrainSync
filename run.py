7cbc7f811b46ae890872297b5920af70f164b856
Sara <sara.rosati@tiscali.it>
2025-05-26 22:36:19 +0200

    Add title screen and welcome message to start the Breainsync game

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import os
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,6 +16,29 @@ SHEET = GSPREAD_CLIENT.open('BrainSync')
 
 question = SHEET.worksheet('question')
 
-data = question.get_all_values()
+# Function to clean the console screen when the next atempt
+def clean_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-print(data)
+def display_title():
+    print("""
+
+ /$$$$$$$                     /$$            /$$$$$$                               
+| $$__  $$                   |__/           /$$__  $$                              
+| $$  \ $$  /$$$$$$  /$$$$$$  /$$ /$$$$$$$ | $$  \__/ /$$   /$$ /$$$$$$$   /$$$$$$$
+| $$$$$$$  /$$__  $$|____  $$| $$| $$__  $$|  $$$$$$ | $$  | $$| $$__  $$ /$$_____/
+| $$__  $$| $$  \__/ /$$$$$$$| $$| $$  \ $$ \____  $$| $$  | $$| $$  \ $$| $$      
+| $$  \ $$| $$      /$$__  $$| $$| $$  | $$ /$$  \ $$| $$  | $$| $$  | $$| $$      
+| $$$$$$$/| $$     |  $$$$$$$| $$| $$  | $$|  $$$$$$/|  $$$$$$$| $$  | $$|  $$$$$$$
+|_______/ |__/      \_______/|__/|__/  |__/ \______/  \____  $$|__/  |__/ \_______/
+                                                      /$$  | $$                    
+                                                     |  $$$$$$/                    
+                                                      \______/                     
+"""
+          )
+    print(">> Welcome to BrainSync: The 3-Level Knowledge Challenge!")
+    print("Your goal: Answer 5 questions correctly to move to the next level.")
+    print("Levels: Easy → Medium → Hard")
+    print("Answer by typing A, B, or C.\n")
+
+display_title()
